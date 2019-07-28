b8fd2215ded7c87b4e81c6fbe1fdf05f43663b9c
Cthnn <ethan@toysdiva.com>
2019-07-28 05:24:37 -0400

    Add Image Functionality Implemented

--- app/src/main/java/com/example/rws/MainActivity.java
+++ app/src/main/java/com/example/rws/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.rws;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Uri imageUri;
+    ImageView imageView;
+    Button addButton;
+    private static final int PICK_IMAGE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButton = (Button)findViewById(R.id.button);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openGallery();
+            }
+        });
+
+    }
+    private void openGallery(){
+        Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
+        gallery.setType("*/*");
+        startActivityForResult(gallery,PICK_IMAGE);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
+            imageUri = data.getData();
+            imageView.setImageURI(imageUri);
+        }
     }
 }
