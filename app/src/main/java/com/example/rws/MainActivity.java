013d22d5eea0bcd095159a3f5bd70de1270cb370
Cthnn <ethan@toysdiva.com>
2019-07-28 13:32:44 -0400

    -Created Dialog for Album Creation -Removed Title Bar -Designed Temporary Layout of Album Creation Dialog

--- app/src/main/java/com/example/rws/MainActivity.java
+++ app/src/main/java/com/example/rws/MainActivity.java
@@ -1,37 +1,67 @@
 package com.example.rws;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
     Uri imageUri;
-    ImageView imageView;
     Button addButton;
     private static final int PICK_IMAGE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.setContentView(R.layout.activity_main);
         addButton = (Button)findViewById(R.id.button);
-        imageView = (ImageView)findViewById(R.id.imageView);
         addButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openGallery();
+                LayoutInflater inflater = (MainActivity.this).getLayoutInflater();
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("Set An Album Name")
+                        .setView(inflater.inflate(R.layout.album_create,null));
+                builder.setPositiveButton("Create", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User clicked OK button
+                    }
+                });
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                    }
+                });
+                AlertDialog dialog = builder.create();
+                dialog.show();
             }
         });
 
     }
-    private void openGallery(){
+
+    private void albumCreation(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Album Name:")
+                .setTitle("Album Name");
+    }
+
+    /*private void openGallery(){
         Intent gallery = new Intent(Intent.ACTION_GET_CONTENT);
-        gallery.setType("*/*");
+        gallery.setType("");
         startActivityForResult(gallery,PICK_IMAGE);
     }
     @Override
@@ -39,7 +69,11 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode,resultCode,data);
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
             imageUri = data.getData();
+            ImageView imageView = new ImageView(MainActivity.this);
+            ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.container);
             imageView.setImageURI(imageUri);
+            layout.addView(imageView);
         }
-    }
+    }*/
+
 }
