ddd75279ff9ab41af15257a96afc375cd0de1ff7
Cthnn <ethan@toysdiva.com>
2019-07-29 15:45:23 -0400

    -Album Class Constructors and mutator methods created -Actual album creation implemented -Update display in Main activity upon album creation

--- app/src/main/java/com/example/rws/Album.java
+++ app/src/main/java/com/example/rws/Album.java
@@ -8,4 +8,24 @@ public class Album {
     protected String name;
     protected Uri image;
     protected ArrayList<Uri> images;
+
+    public Album(ArrayList<Uri> images){
+        this.images = images;
+        image = this.images.get(0);
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setImage(Uri image){
+        this.image = image;
+    }
+    public String getName(){
+        return name;
+    }
+    public Uri getImage(){
+        return image;
+    }
+    public ArrayList<Uri> getImages(){
+        return images;
+    }
 }
--- app/src/main/java/com/example/rws/MainActivity.java
+++ app/src/main/java/com/example/rws/MainActivity.java
@@ -18,12 +18,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
-    Uri imageUri;
+
     Button menuButton;
     Button aButton;
     Button rButton;
     Button tButton;
+    ArrayList<Album> albumList = new ArrayList<>();
 
     private static final int PICK_IMAGE = 100;
     @Override
@@ -75,7 +78,8 @@ public class MainActivity extends Activity {
 
     }
     private void albumCreation(){
-        LayoutInflater inflater = (MainActivity.this).getLayoutInflater();
+        final LayoutInflater inflater = (MainActivity.this).getLayoutInflater();
+        final View infView =inflater.inflate(R.layout.activity_album_page,null);
         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
         builder.setTitle("Set An Album Name")
                 .setView(inflater.inflate(R.layout.album_create,null));
@@ -83,6 +87,12 @@ public class MainActivity extends Activity {
             public void onClick(DialogInterface dialog, int id) {
                 Intent myIntent = new Intent(MainActivity.this, AlbumPage.class);
                 MainActivity.this.startActivity(myIntent);
+                openGallery();
+                Album last = albumList.get(albumList.size()-1);
+                EditText nameField = infView.findViewById(R.id.nameField);
+                last.setName(nameField.getText().toString());
+                albumCreated();
+
             }
         });
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
@@ -103,12 +113,17 @@ public class MainActivity extends Activity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
         super.onActivityResult(requestCode,resultCode,data);
         if(resultCode == RESULT_OK && requestCode == PICK_IMAGE){
-            imageUri = data.getData();
+            ArrayList<Uri> images = new ArrayList<>();
+            Uri imageUri = data.getData();
+            images.add(imageUri);
+        }
+    }
+    private void albumCreated(){
+        for(Album album:albumList){
             ImageView imageView = new ImageView(MainActivity.this);
             ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.container);
-            imageView.setImageURI(imageUri);
+            imageView.setImageURI(album.getImage());
             layout.addView(imageView);
         }
     }
-
 }
